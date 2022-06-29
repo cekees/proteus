ab1eb107d8bc013c2db25ba84304279e42f26e2a
cekees <613654+cekees@users.noreply.github.com>
2022-06-28 20:20:00 -0500

    switched mike3 to use mvapich2

--- proteus/config/mike.py
+++ proteus/config/mike.py
@@ -1,7 +1,7 @@
 from .default import *
 
-PROTEUS_MPI_INCLUDE_DIR = "/usr/local/packages/mpich/3.4.2/s3bq4iw/include"
-PROTEUS_MPI_LIB_DIR = "/usr/local/packages/mpich/3.4.2/s3bq4iw/lib"
+PROTEUS_MPI_INCLUDE_DIR = "/usr/local/packages/mvapich2/2.3.7/intel-2021.5.0/include"
+PROTEUS_MPI_LIB_DIR = "/usr/local/packages/mvapich2/2.3.7/intel-2021.5.0/lib"
 PROTEUS_MPI_INCLUDE_DIRS = [PROTEUS_MPI_INCLUDE_DIR]
 PROTEUS_MPI_LIB_DIRS = [PROTEUS_MPI_LIB_DIR]
 PROTEUS_SCOREC_LIB_DIRS += PROTEUS_MPI_LIB_DIRS
