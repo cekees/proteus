65b66b9190fa392d1936bee4dddcc3f56fe2dbcf
cekees <613654+cekees@users.noreply.github.com>
2025-08-07 14:53:14 -0500

    add scripts supporting Leveque and Li test problems.

---
+++ test/ci/ex4.1.sh
@@ -0,0 +1,24 @@
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=1" > ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=2" >> ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=3" >> ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=4" >> ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=5" >> ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=False refinement=6" >> ex4.1r.out
+grep L2 ex4.1r.out
+grep Linfty ex4.1r.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=1" > ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=2" >> ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=3" >> ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=4" >> ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=5" >> ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True skew=0.0 refinement=6" >> ex4.1.out
+grep L2 ex4.1.out
+grep Linfty ex4.1.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=1" > ex4.1us.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=2" >> ex4.1us.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=3" >> ex4.1us.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=4" >> ex4.1us.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=5" >> ex4.1us.out
+parun ladr_ss_2d_p.py ladr_ss_2d_c0p1_n.py -l 5 -v -C "test=4.1 unstructured=True refinement=6" >> ex4.1us.out
+grep L2 ex4.1us.out
+grep Linfty ex4.1us.out
